fc6f997fcbf84600cdb3e24c13074378a431fc61
yewon-yw <94778694+KimGiheung@users.noreply.github.com>
2024-03-28 12:54:25 +0900

    Update opencv_ex.py

--- code/opencv_ex.py
+++ code/opencv_ex.py
@@ -14,8 +14,7 @@ def Image_display(img):
     cv.imshow('Image Display', img)
     #pixcel의 rbg값 출력
     print(img[0,0,0], img[0,0,1], img[0,0,2])
-    cv.waitKey()
-    cv.destroyAllWindows()
+
 
 # 4. 영상 형태 변환하고 크기 축소하기
 def grayNresize(img):
@@ -31,10 +30,31 @@ def grayNresize(img):
     cv.imshow('Gray image', gray_scale)
     cv.imshow('Gray small image', gray_small)
 
-    cv.waitKey()
-    cv.destroyAllWindows()
 
-
+# 7. 영상에 도형을 그리고 글씨 쓰기
+def draw(event, x, y, flags, param): #call function
+    if event == cv.EVENT_LBUTTIONDOWN: # 마우스 왼쪽 버튼 클릭됐을 때
+        # cursor 위치에 (200,200) 크기의 빨간색 선 두께 2px의 정사각형을 그림
+        cv.rectangle(img, (x, y), (x+200, y+200), (0,0,255),2)
+    elif event == cv.EVENT_RBUTTIONDOWN: # 마우스 오른쪽 버튼 클릭됐을 때
+        # cursor 위치에 (100,100) 크기의 파란색 선 두께 2px의 정사각형을 그림
+        cv.rectangle(img, (x, y), (x+100, y+100), (255,0,0),2)
+    cv.namedWindow('Drawing')
+    cv.imshow('Drawing', img)
+ 
 if __name__ == '__main__':
     Image_display(img)
     grayNresize(img)
+
+    # cv.waitKey()
+    # cv.destroyAllWindows()
+
+    #'Drawing' 윈도우에 draw 콜백 함수 지정해 줌
+    cv.namedWindow('Drawing')
+    cv.setMouseCallback('Drawing', draw)
+
+    while True:
+        if cv.waitKey(1)==ord('q'):
+            cv.destroyAllWindows()
+            break
+
